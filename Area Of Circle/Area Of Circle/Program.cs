e8ed9a516f91e68313213079c0a283c167d38773
Gurman Singh <95881451+GurmanSinghh@users.noreply.github.com>
2022-12-05 12:53:23 -0500

    First Commit

---
+++ Area Of Circle/Area Of Circle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Area_Of_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             /* Write a program to calculate the area of a circle. The user will enter the radius of the circle 
+                and the program will calculate and display the area according to the formula (area = 3.14 * 
+                radius * radius). You must accept fractions as the input. If the user enters 1.2 for the radius 
+                then the area will be 4.52. */ 
+             
+             //Input
+
+            double radius, area;
+
+            Console.Write("Enter The Radius:");
+
+            //Processing
+
+            radius = double.Parse(Console.ReadLine());
+            area = 3.14 * radius * radius;
+
+            //Output
+
+            Console.WriteLine("The Area is " + area);
+
+        }
+    }
+}
---
+++ Ticket Cost/Ticket Cost/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ticket_Cost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Adult ticket cost $3.75 and child ticket cost $2.25. Write a program to prompt the user for 
+               the amount of adult and child ticket that she needs. The program will display a user-friendly
+               message of the number of tickets brought as well as the total cost. (Use the "C" formatstring for currency) */
+            
+            //Input
+
+            double adultTicket = 3.75, childTicket = 2.25, totalPrice;
+            int totalChildTickets, totalAdultTickets;
+
+            Console.Write("Enter The Number Of Adult Tickets: ");
+            totalAdultTickets = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter The Numeber Of Child Tickets: ");
+            totalChildTickets = int.Parse(Console.ReadLine());
+            
+            //Processing
+
+            totalPrice = adultTicket * totalAdultTickets + childTicket * totalChildTickets;
+
+            //Output
+
+            Console.WriteLine();
+            Console.WriteLine("Order Summary: ");
+            Console.WriteLine();
+            Console.WriteLine("Number Of Adult Tickets: " + totalAdultTickets + " ($3.75 Each)");
+            Console.WriteLine("Number Of Child Tickets: " + totalChildTickets + " ($2.25 Each)");
+            Console.WriteLine();
+            Console.WriteLine($"Total Price: {totalPrice.ToString("c")}");
+
+        }
+    }
+}
